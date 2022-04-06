8b9bd6319bd4969ed81c2e4299cb8ad81c336af2
isabel-noh <nohhyunjeong93@gmail.com>
2022-04-07 06:29:16 +0900

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,27 +1,50 @@
 //로그인
 import React from "react";
 import styled from 'styled-components';
-import { useHistory } from "react-router-dom";
 import Button from '@mui/material/Button';
 import { Input } from "../elements";
+import { getCookie, setCookie, deleteCookie } from "../shared/Cookie";
+import { useDispatch, useSelector } from "react-redux";
+import { actionCreators as userActions } from "../redux/modules/user";
+import { loginFB } from "../redux/modules/user"
 
-const Login = () => {
-    const history = useHistory();
+const Login = (props) => {
+    const dispatch = useDispatch();
+    
+    const [id, setId] = React.useState();
+    const [pwd, setPwd] = React.useState();
+
+    const loginID = (e) => {
+        setId(e.target.value);
+    }
+    const loginPWD = (e) => {
+        setPwd(e.target.value);
+    }
+
+    const logIn = () => {
+        if(id === '' || pwd === ''){
+            window.alert("아이디 혹은 비밀번호를 입력해주세요.");
+            return;
+        }
+        dispatch(userActions.loginFB(id, pwd));
+    }
 
     return (
         <Wrap>
             <h1>Log In</h1>
             <div>
                 <div>
-                    <label>ID</label><br />
-                    <Input placeholder="Please enter your ID" ></Input><br />
-                    <label>Password</label><br />
-                    <Input placeholder="Please enter your Password" ></Input><br />
+                    <label htmlFor="id">ID</label><br />
+                    <Input id="id" value={id} _onChange={loginID} type="text" placeholder="Please enter your ID" /><br />
+                    <label htmlFor="password">Password</label><br />
+                    <Input id="password" value={pwd} _onChange={loginPWD} type="password" placeholder="Please enter your Password"/><br />
                 </div>
                 <br />
 
                 <div>
-                    <Button onClick={() => {history.push("/");}} variant="contained" color="secondary">Log In</Button>
+                    <Button style={{width: "300px"}}  
+                            variant="contained" color="secondary"
+                            onClick={() => {logIn()}}>Log In</Button>
                 </div>
             </div>
         </Wrap>
@@ -29,7 +52,8 @@ const Login = () => {
 }
 
 const Wrap = styled.div`
-    
+    width: 300px;
+    margin: 100px auto;
 `
 
 export default Login;
