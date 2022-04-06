47376712193ce7242a6543d15ad9dd793ffb3ce4
isabel-noh <nohhyunjeong93@gmail.com>
2022-04-07 05:51:32 +0900

    Update Cookie.js
    setCookie, getCookie, deleteCookie

--- src/shared/Cookie.js
+++ src/shared/Cookie.js
@@ -1,22 +1,28 @@
 import React from "react";
 
 const getCookie = (name) => {
-    let value = "; " + document.cookie;
-    console.log(value);
-    let parts = value.split("; " + name + "=");
-    console.log(parts);
-    if(parts.length.length === 2){
-        return parts.pop().split(";").shift();
+    //선생님코드
+    //let value = "; " + document.cookie; 
+    //let parts = value.split(`; ${name}=`); 
+
+    let value = document.cookie; 
+    let parts = value.split(`${name}=`); // 수많은 key=value형식의 cookie들 가운데에서 "; name="을 기준으로 앞 뒤를 잘라서 length=2인 배열만듦
+                                        //aaa=bbb; name=ddd; eee=fff; ... > [aaa=bbb; / ddd; eee=fff; ... ]
+    if ( parts.length === 2 ){
+        return parts.pop().split(";").shift(); // parts배열에서 맨뒤의 값만 뽑아서 ';'를 기준으로 나누고, 나눈 배열에서 맨앞의 값을 return
+        //ddd; eee=fff; ... > [ddd, eee=fff; ... ] > return ddd;
     }
-};
-const setCookie = (name, value, exp = 5)=> {
-    let date = new Date();
-    date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
-    document.cookie = `${name} = ${value}; expires = ${date.toUTCString()}; path=/`;
-} 
-//쿠키를 지우는 방법 > 쿠키의 expires 날짜를 엄청 과거로 돌리면 새로고침시 쿠키가 삭제됨
-const deleteCookie = (name) => {
-    document.cookie = name + '=; expires = Thu, 01 Jan 1999 00:00:10 GMT';
+    
 }
+const setCookie = (name, value, expire = 5) => { //expire=5 >>expire을 인자로 받아오지 않아도 기본값5로 setCookie에서 파라미터로 쓸 수 있게 해줌
+    let date = new Date(); // Date객체로 날짜 받아옴 
+    date.setTime(date.getTime() + expire * 1000 * 60 * 60 * 24); //3일뒤로 계산
+    document.cookie = `${name} = ${value}; expires=${date.toUTCString}`; // 설정한 값 string으로 바꿔서 expires로 설정
+}
+const deleteCookie = (name) => {
+    let date = new Date('1999-01-01').toUTCString();
+    document.cookie = name+"=; expires="+date;  // value값을 비워버린거임 (삭제)
+}
+
 
 export {getCookie, setCookie, deleteCookie};
